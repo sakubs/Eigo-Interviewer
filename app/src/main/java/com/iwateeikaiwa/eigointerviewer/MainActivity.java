79e9f45a340923ef06e042f0ced4572bc333419f
Brian Sakurada <sakuradabs@gmail.com>
2019-09-30 09:34:50 +0900

    Added some formatting to the question display.

--- app/src/main/java/com/iwateeikaiwa/eigointerviewer/MainActivity.java
+++ app/src/main/java/com/iwateeikaiwa/eigointerviewer/MainActivity.java
@@ -3,12 +3,64 @@ package com.iwateeikaiwa.eigointerviewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mNextButton;
+    private Button mHintButton;
+    private Button mFinishButton;
+    private TextView mContentView;
+    private ArrayList<String> mQuestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mQuestions = new ArrayList<String>(
+                Arrays.asList(getResources().getStringArray(R.array.questions)
+                )
+        );
+
+        mNextButton = (Button) findViewById(R.id.btn_next);
+        mHintButton = (Button) findViewById(R.id.btn_hint);
+        mFinishButton = (Button) findViewById(R.id.btn_finish);
+        mContentView = (TextView) findViewById(R.id.tv_content);
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show a random question.
+
+                mContentView.setText(getFact());
+            }
+        });
+
+        mHintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mFinishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public String getFact() {
+
+        Random random = new Random();
+        return mQuestions.get(random.nextInt(mQuestions.size()));
     }
 }
